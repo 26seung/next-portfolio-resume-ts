0eff8d28ab8a922bfbdb10e7072d64e5241e8ecc
Euseung <yousi7@naver.com>
2023-09-20 23:44:18 +0900

    feat: add next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: [
+      "s3.us-west-2.amazonaws.com",
+      "images.unsplash.com",
+      "www.notion.so",
+    ],
+    format: ["image/png", "image/webp", "image/jpeg"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
