23c51acaea869507d4ba592453aa55ba4b8af022
Euseung <yousi7@naver.com>
2023-09-21 23:30:02 +0900

    feat: add components

---
+++ components/skills.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { dataSkills } from "@/lib/constants";
+import React from "react";
+import SectionHeading from "./section-heading";
+import { useSectionInView } from "@/lib/hooks";
+
+const Skills = () => {
+  const { ref } = useSectionInView("Skills");
+  return (
+    <section
+      id="skills"
+      ref={ref}
+      className="max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+    >
+      <SectionHeading>My skills</SectionHeading>
+      <div className="flex flex-wrap justify-center gap-2 text-lg text-gray-800 mb-32">
+        {dataSkills.map((skill, index) => (
+          <div
+            className="bg-white border border-black/20 rounded-xl px-5 py-3 focus:scale-125 hover:scale-125 active:scale-125 transition cursor-pointer dark:bg-white/10 dark:text-white/80"
+            key={index}
+            // variants={fadeInAnimationVariants}
+            // initial="initial"
+            // whileInView="animate"
+            // viewport={{
+            //   once: true,
+            // }}
+            // custom={index}
+          >
+            {skill}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
