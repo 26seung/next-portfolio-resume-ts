8bbda5b0b8e2bf971047bd273c287449d5d74d0e
Euseung <yousi7@naver.com>
2023-10-03 23:36:56 +0900

    feat: add lib

---
+++ lib/constants.ts
@@ -0,0 +1,42 @@
+export const dataHeader = [
+  {
+    label: "Home",
+    hash: "#home",
+  },
+  {
+    label: "About",
+    hash: "#about",
+  },
+  {
+    label: "Projects",
+    hash: "#projects",
+  },
+  {
+    label: "Skills",
+    hash: "#skills",
+  },
+  {
+    label: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const dataSkills = [
+  "JAVA",
+  "SpringBoot",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Redux",
+  "Next.js",
+  "Node.js",
+  "Tailwind",
+  "JPA",
+  "Prisma",
+  "MongoDB",
+  "Mysql",
+  "Redis",
+  "Git",
+  "Docker",
+  "AWS",
+] as const;
